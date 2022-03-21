546acbbcaf3caf081991652554084d6b8334a2a6
Collin Modelski <cmm0216@auburn.edu>
2022-03-20 23:59:20 -0500

    Updated wordsearch.txt and Origin_Search.py to only find exact word matches that are case insensitive!

--- Origin_Search.py
+++ Origin_Search.py
@@ -2,7 +2,7 @@
 import re
 import sys
 
-string = r'.herit.'
+string = r'(?i)..herit\w+'
 object = re.compile(string)
 
 print('Opening origin.txt')
@@ -14,7 +14,9 @@ with open('origin.txt', 'r') as in_stream:
 			line = line.strip()
 			word_list = line.split()
 			if object.search(line):
-				print(word_line, "\t", line, file = out_stream)
+				word = object.search(line)
+				#print(word_line, "\t", word.group())
+				print(word_line, "\t", word.group(), file = out_stream)
 			word_line += 1
 print("Done!")
 print('origin.txt is closed?', in_stream.closed)
